d24d065d0f638d52853540d6e53c96230001ea9a
Ivan Tishko <ivantishko29@gmail.com>
2025-03-31 13:32:50 +0300

    fix: исправлены URL-маршруты для работы с постами и категориями

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -6,7 +6,11 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='index'),
-    path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
+    path(
+        'posts/<int:pk>/',
+        views.PostDetailView.as_view(),
+        name='post_detail'
+    ),
     path(
         'category/<slug:slug>/',
         views.CategoryPostsView.as_view(),
