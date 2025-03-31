effbef3e9f5b0b6d777207d5cdfbdf2e4efd3fb1
Ivan Tishko <ivantishko29@gmail.com>
2025-03-31 13:30:39 +0300

    fix: исправлены параметры URL-маршрутов для соответствия представлениям

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -5,16 +5,11 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('posts/<int:id>/', views.post_detail, name='post_detail'),
+    path('', views.HomePageView.as_view(), name='index'),
+    path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
     path(
-        'category/<slug:category_slug>/',
-        views.category_posts,
+        'category/<slug:slug>/',
+        views.CategoryPostsView.as_view(),
         name='category_posts'
-    ),
-    path(
-        'location/<slug:location_slug>/',
-        views.location_posts,
-        name='location_posts'
-    ),
+    )
 ]
